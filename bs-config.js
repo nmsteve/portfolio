2fbefeb616af2cc7b719293f2c979f422b6c0188
git config --global user.name Njugi <nmsteve45@gmail.com>
2022-02-16 15:37:32 +0300

    adding port

--- bs-config.js
+++ bs-config.js
@@ -9,6 +9,7 @@ module.exports = {
   injectChanges: false, // workaround for Angular 2 styleUrls loading
   files: ['./**/*.{html,htm,css,js}'],
   open: false,
+  port: process.env.PORT|| 3000,
   watchOptions: {
     ignored: 'node_modules'
   },
